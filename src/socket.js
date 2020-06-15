c5a7fe37fd1edfe7ff97bc100f812d2571f96772
Anthony Farncombe <anthony.farncombe@redpack.co.uk>
2020-06-15 11:41:30 +0100

    Update and tidy up npm packages

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import io from "socket.io-client";
 import store from "./store";
 
-const socket = io("http://localhost:3000");
+const socket = io("https://postwick-api.heathwood.org.uk");
 
 socket.on("connect", () => {
   console.log("socket.io connected");
